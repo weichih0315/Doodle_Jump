eb3585c3ca2adc7ced763783014c5091dba70543
weichih0315 <53685770+weichih0315@users.noreply.github.com>
2019-08-04 23:54:58 +0800

    Demo用

---
+++ doodle jump/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed = .3f;
+
+    private Vector3 currentVelocity;
+
+    public static event System.Action OnChangeHeighStatic;
+
+    bool onEnable = true;
+
+    Vector3 gameOverPoint;
+
+    Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        player.OnDeath += GameOver;
+    }
+
+    void LateUpdate () {
+        if (onEnable)
+        {
+            if (target.position.y > transform.position.y)
+            {
+                Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+                //transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+                transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed * Time.deltaTime);
+
+                if (OnChangeHeighStatic != null)
+                {
+                    OnChangeHeighStatic();
+                }
+
+            }
+        }
+        else
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, gameOverPoint, ref currentVelocity, 20f * Time.deltaTime);
+        }
+	}
+
+    void GameOver()
+    {
+        gameOverPoint = transform.position - Vector3.up * 10;
+        onEnable = false;
+    }
+}
---
+++ doodle jump/Assets/Scripts/Menu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject mainMenuHolder;
+	public GameObject optionsMenuHolder;
+
+    public Text masterVolumeText;
+    public Text musicVolumeText;
+    public Text sfxVolumeText;
+
+    public Slider[] volumeSliders;
+
+	void Start() {
+		volumeSliders [0].value = AudioManager.instance.masterVolumePercent;
+		volumeSliders [1].value = AudioManager.instance.musicVolumePercent;
+		volumeSliders [2].value = AudioManager.instance.sfxVolumePercent;
+
+        masterVolumeText.text = (int)(volumeSliders[0].value * 100) + "";
+        musicVolumeText.text = (int)(volumeSliders[1].value * 100) + "";
+        sfxVolumeText.text = (int)(volumeSliders[2].value * 100) + "";
+
+    }
+    
+	public void Play() {
+		SceneManager.LoadScene ("Game");
+	}
+
+    public void Quit() {
+		Application.Quit ();
+	}
+
+	public void OptionsMenu() {
+		mainMenuHolder.SetActive (false);
+		optionsMenuHolder.SetActive (true);
+	}
+
+	public void MainMenu() {
+		mainMenuHolder.SetActive (true);
+		optionsMenuHolder.SetActive (false);
+	}
+
+	
+
+	public void SetMasterVolume(float value) {
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Master);
+        masterVolumeText.text = (int)(value * 100) + "";
+    }
+
+	public void SetMusicVolume(float value) {
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Music);
+        musicVolumeText.text = (int)(value * 100) + "";
+    }
+
+	public void SetSfxVolume(float value) {
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Sfx);
+        sfxVolumeText.text = (int)(value * 100) + "";
+    }
+
+}
