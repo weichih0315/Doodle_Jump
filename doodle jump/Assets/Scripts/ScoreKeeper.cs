82ee642af9f3629e729c20d477d111afe8cb956a
weichih0315 <53685770+weichih0315@users.noreply.github.com>
2019-08-05 10:34:54 +0800

    Demo

---
+++ doodle jump/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public static int score { get; private set; }
+
+	void Start() {
+
+        CameraTarget.OnChangeHeighStatic += OnChangeHeigh;
+
+    }
+
+    void OnChangeHeigh()
+    {
+        score += 10;
+    }
+
+    private void OnDisable()
+    {
+        CameraTarget.OnChangeHeighStatic -= OnChangeHeigh;
+    }
+}
